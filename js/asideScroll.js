98e1daaaba98a9a2bf153a0518131429c2486f12
thunderFlydge <thunderFlydge@yandex.ru>
2018-04-05 18:56:02 +0300

    added: scroll for central content

--- js/asideScroll.js
+++ js/asideScroll.js
@@ -2,45 +2,50 @@
 let clientHeight = document.documentElement.clientHeight;
 let leftBlock = document.querySelector('.left-block');
 let rightBlock = document.querySelector('.right-block');
+let centralBlock = document.querySelector('.central-block');
+let centralBlockChild = document.querySelector('.central-block-child');
 leftBlock.style.setProperty('--leftBlockHeight', (clientHeight-47) + 'px');
-rightBlock.style.setProperty('--asideBlockHeight', (clientHeight-47) + 'px');
+rightBlock.style.setProperty('--rightBlockHeight', (clientHeight-47) + 'px');
+centralBlock.style.setProperty('--centralBlockHeight', (clientHeight-47) + 'px');
+centralBlockChild.style.setProperty('--centralBlockChildHeight', (clientHeight-47) + 'px');
 
 
 
 
 let clientWidth = document.documentElement.clientWidth;
-let centralBlock = document.querySelector('.central-block');
 let cardsFoundBlock = document.querySelector('.cardsFound');
-
 let centralBlockWidth = centralBlock.clientWidth;
 let leftBlockWidth = leftBlock.clientWidth + 18.5;
-let rightBlockWidth = rightBlock.clientWidth;
+let rightBlockWidth = rightBlock.clientWidth + 18.5;
+
 console.log('centralBlock',centralBlockWidth);
 console.log('leftBlock',leftBlockWidth);
 console.log('rightBlock',rightBlockWidth);
 console.log('clientWidth',clientWidth);
 console.log(centralBlockWidth- leftBlockWidth - rightBlockWidth + ':centralBlockWidth- leftBlockWidth - rightBlockWidth')
-//alert(asideBlock2.clientWidth + ' | ' + asideBlock.clientWidth + '=' + (asideBlock2.clientWidth+ asideBlock.clientWidth));
-//centralBlock.style.setProperty('--centralBlockWidth', (clientWidth- leftBlockWidth - rightBlockWidth) + 'px');
-//centralBlock.style.setProperty('--centralBlockWidth', 800 + 'px');
-//rightBlock.style.setProperty('--rightBlockWidth', (clientWidth-centralBlockWidth+leftBlockWidth) + 'px');
-//rightBlock.style.setProperty('--rightBlockWidth', 600 + 'px');
 
-cardsFoundBlock.style.setProperty('--cardsFoundWidth', ((leftBlockWidth + rightBlockWidth) / (centralBlockWidth / 100) )  + '%' );
+
+//let cardsFoundPercent = (centralBlockWidth - (leftBlockWidth + rightBlockWidth)) / (centralBlockWidth / 100) - 3.2;
+let cardsFoundPercent = (centralBlockWidth - (leftBlockWidth + rightBlockWidth + 100));
+cardsFoundBlock.style.setProperty('--cardsFoundWidth', cardsFoundPercent  + 'px' );
 centralBlock.style.setProperty('--centralBlockLeftPadding',leftBlockWidth + 'px');
-centralBlock.style.setProperty('--centralBlockRightPadding',rightBlockWidth + 'px');
+centralBlockChild.style.setProperty('--centralBlockRightPadding',rightBlockWidth + 'px');
+
 console.log(((centralBlockWidth - (leftBlockWidth + rightBlockWidth)) / (centralBlockWidth / 100) )  + '%');
 
 
 
 new ResizeSensor(jQuery('.left-block'), function(){
 
-    let clientWidth = document.documentElement.clientWidth;
+    let centralBlockWidth = centralBlock.clientWidth;
     let leftBlockWidth = leftBlock.clientWidth + 18.5;
-    let rightBlockWidth = rightBlock.clientWidth;
+    let rightBlockWidth = rightBlock.clientWidth + 18.5;
+    let cardsFoundPercent = (centralBlockWidth - (leftBlockWidth + rightBlockWidth + 100));
 
     centralBlock.style.setProperty('--centralBlockLeftPadding',leftBlockWidth + 'px');
-    centralBlock.style.setProperty('--centralBlockRightPadding',rightBlockWidth + 'px');
+    centralBlockChild.style.setProperty('--centralBlockRightPadding',rightBlockWidth + 'px');
+    cardsFoundBlock.style.setProperty('--cardsFoundWidth', cardsFoundPercent  + 'px' );
+
 
 
 
@@ -48,12 +53,15 @@ new ResizeSensor(jQuery('.left-block'), function(){
 
 new ResizeSensor(jQuery('.right-block'), function(){
 
-    let clientWidth = document.documentElement.clientWidth;
+    let centralBlockWidth = centralBlock.clientWidth;
     let leftBlockWidth = leftBlock.clientWidth + 18.5;
-    let rightBlockWidth = rightBlock.clientWidth;
+    let rightBlockWidth = rightBlock.clientWidth + 18.5 ;
+    let cardsFoundPercent = (centralBlockWidth - (leftBlockWidth + rightBlockWidth + 100));
 
     centralBlock.style.setProperty('--centralBlockLeftPadding',leftBlockWidth + 'px');
-    centralBlock.style.setProperty('--centralBlockRightPadding',rightBlockWidth + 'px');
+    centralBlockChild.style.setProperty('--centralBlockRightPadding',rightBlockWidth + 'px');
+    cardsFoundBlock.style.setProperty('--cardsFoundWidth', cardsFoundPercent  + 'px' );
+
 
 
 });
