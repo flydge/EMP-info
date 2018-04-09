072151bd51522b613e88f82158b0d7ec7b459a22
thunderFlydge <thunderFlydge@yandex.ru>
2018-04-09 20:24:44 +0300

    right padding for aside menu appdated

--- js/header.js
+++ js/header.js
@@ -1,20 +1,3 @@
-/*
-$(".tabs").click(function(e) {
-    let target = e.target;
-    //console.log(target.parentElement.tagName);
-    if($(target.parentNode.parentNode).hasClass("activeTab")){
-    }else {
-        let childs = target.parentNode.parentNode.parentNode.childNodes;
-        for (let item of childs){
-            $(item).removeClass("activeTab");
-        }
-        if (target.parentElement.tagName !== "LI" && target.parentElement.tagName !== "UL"){
-            $(target.parentNode.parentNode).addClass("activeTab");
-        }
-
-    }
-});
-*/
 
 $(".tabs").hover(
     function () {
