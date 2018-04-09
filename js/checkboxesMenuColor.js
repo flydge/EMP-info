48dfce5b95e5bcfff19d042b158472495b59e29e
thunderFlydge <thunderFlydge@yandex.ru>
2018-04-09 16:10:45 +0300

    added: checkbox logic        central icons position logic        small scrolls

--- js/checkboxesMenuColor.js
+++ js/checkboxesMenuColor.js
@@ -1,6 +1,7 @@
 $('.form-check-input').click(function(e) {
     let target = e.target;
-    //console.log('tagName: ', target.parentNode.parentNode.parentNode.nodeName)
+    let childs = $(target.parentNode.parentNode.parentNode).find('.form-check-input');
+
     if($(target.parentNode).hasClass('form-check-right')){
         if($(target.parentNode).hasClass("activeSection")) {
             $(target.parentNode).removeClass("activeSection");
@@ -8,10 +9,21 @@ $('.form-check-input').click(function(e) {
             $(target.parentNode).addClass("activeSection");
         }
     }else {
+
         if($(target.parentNode.parentNode.parentNode).hasClass("activeSection")) {
+
+            for (let item of childs){
+                item.checked = false
+            }
             $(target.parentNode.parentNode.parentNode).removeClass("activeSection");
+            $(target.parentNode.parentNode.parentNode).find('details').removeClass("activeSection");
+            $(target.parentNode.parentNode.parentNode).find('div').removeClass("activeSection");
+
         }else {
             $(target.parentNode.parentNode.parentNode).addClass("activeSection");
+            for (let item of childs){
+                item.checked = true
+            }
         }
     }
 });
